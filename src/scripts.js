364d4d4bcb12be3d3c121987dd1392ea89c81269
szyszak <krzszyszko@gmail.com>
2017-02-20 19:48:36 +0100

    added script changing active class on menu items on click

--- src/scripts.js
+++ src/scripts.js
@@ -6,3 +6,21 @@ const menuTrigger = document.querySelector(".nav-toggle");
 menuTrigger.addEventListener("click", () => {
   navMenu.classList.toggle("nav-open");
 });
+
+function cl(text) {
+  console.log(text);
+};
+
+const links = document.querySelectorAll(".nav-menu li a");
+
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", setActiveClass);
+};
+
+function setActiveClass() {
+  for (let link of links) {
+    link.classList.remove("active");
+  }
+
+  this.classList.add("active");
+};
