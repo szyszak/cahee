f0da6b9714f29bc4ccc70d8e945385da609f3563
szyszak <krzszyszko@gmail.com>
2018-03-02 21:22:05 +0100

    added smooth scrolling

--- dist/scripts.js
+++ dist/scripts.js
@@ -2,22 +2,27 @@
 
 const navMenu = document.querySelector(".nav-menu");
 const menuTrigger = document.querySelector(".nav-toggle");
+const navLinks = document.querySelectorAll(".nav-menu li a");
 
 menuTrigger.addEventListener("click", () => {
   navMenu.classList.toggle("nav-open");
 });
 
-const links = document.querySelectorAll(".nav-menu li a");
+function smoothScroll(target) {
+  document.querySelector(target).scrollIntoView({ 
+    behavior: 'smooth' 
+  });
+}
 
-for (let i = 0; i < links.length; i++) {
-  links[i].addEventListener("click", clickHandler);
-};
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click", (ev) => {
+    ev.preventDefault();
+    smoothScroll(ev.target.hash);
+    for (let link of navLinks) {
+      link.classList.remove("active");
+    }
 
-function clickHandler() {
-  for (let link of links) {
-    link.classList.remove("active");
-  }
-
-  navMenu.classList.remove("nav-open");
-  this.classList.add("active");
-};
+    navMenu.classList.remove("nav-open");
+    ev.target.classList.add("active");
+  });
+}
